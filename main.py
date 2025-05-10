e799eacc770563e523e5be457f8d10599b4c10a6
Ahmed-Mahmoud-M <ahmedmahmoud842003@gmail.com>
2025-05-10 21:30:42 +0300

    set up env

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from settings import Settings
+
+
+
+pygame.init()
+
+settings = Settings()
+
+screen = pygame.display.set_mode((settings.WIDTH,settings.HEIGHT))
+pygame.display.set_caption("Space Invaders")
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
